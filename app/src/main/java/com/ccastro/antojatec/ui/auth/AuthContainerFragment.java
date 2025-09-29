90cdd121dfb88d25bc638ed58efa8a7b2647e986
Guillermo Castro <cguillermo12@outlook.com>
2025-09-29 09:47:26 -0700

    Reparamos el boton back del AuthContainer ✅

--- app/src/main/java/com/ccastro/antojatec/ui/auth/AuthContainerFragment.java
+++ app/src/main/java/com/ccastro/antojatec/ui/auth/AuthContainerFragment.java
@@ -2,7 +2,10 @@ package com.ccastro.antojatec.ui.auth;
 
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +18,29 @@ public class AuthContainerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_auth_container, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_auth_container, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Obtén el NavController del NavHostFragment dentro de este container
+        NavHostFragment navHostFragment = (NavHostFragment)
+                getChildFragmentManager().findFragmentById(R.id.auth_nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        // Manejo del botón back dentro de las views.
+        requireActivity().getOnBackPressedDispatcher().addCallback(
+                getViewLifecycleOwner(),
+                new OnBackPressedCallback(true) {
+                    @Override
+                    public void handleOnBackPressed() {
+                        if (!navController.popBackStack()) {
+                            setEnabled(false);
+                            requireActivity().onBackPressed();
+                        }
+                    }
+                });
     }
 }
